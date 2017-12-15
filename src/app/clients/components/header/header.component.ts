57a79998eaa4b4619f68de6e2831612faac3e561
unknown <lalang gumirang>
2017-12-15 14:44:46 +0700

    profile

--- src/app/clients/components/header/header.component.ts
+++ src/app/clients/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,25 @@ export class HeaderComponent implements OnInit {
   }
 
   logout(){
-    alert('logout');
+    //alert('logout');
+    swal({
+  title: 'Alert',
+  text: "Apakah Anda akan keluar dari Account Area.",
+  type: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  cancelButtonText: 'Tidak',
+  confirmButtonText: 'Iya'
+}).then((result) => {
+  if (result.value) {
+    swal(
+      'Success!',
+      'Anda sudah keluar dari Account Area.',
+      'success'
+    )
+  }
+})
   }
 
 }
