fc0a0aca85b876838e7ad97c2cae3678be2b7954
budi.kusuma <bdkusuma@gmail.com>
2017-12-20 16:03:10 +0700

    select propinsi, kota, kecamatan, desa, postal @profile page

--- src/app/clients/pages/seller/info-perusahaan/info-perusahaan.component.ts
+++ src/app/clients/pages/seller/info-perusahaan/info-perusahaan.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../../../../servers/service/search/search.service';
 
 @Component({
   selector: 'app-info-perusahaan',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoPerusahaanComponent implements OnInit {
 
-  constructor() { }
+  constructor(private searchService: SearchService) { }
+
+  propinsi = [];
+  city = [];
+  kelurahan = [];
+  selectedProvince: string;
 
   ngOnInit() {
+    this.getPropinsi();
   }
 
+  getPropinsi() {
+    this.searchService.searchProvince('209').subscribe(data => {
+      this.propinsi = data;
+    });
+  }
+
+  selectCity(id) {
+    this.searchService.searchCity(id).subscribe(data => {
+      this.city = data;
+    });
+  }
+
+  selectKelurahan(id) {
+    this.searchService.searchKelurahan(id).subscribe(data => {
+      this.kelurahan = data;
+    });
+  }
 }
