3f3939873fd95cc5c8f5c27a9fa9bf0e8344a536
unknown <lalang gumirang>
2017-12-20 16:01:36 +0700

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -65,6 +65,7 @@ import { FrontHeaderComponent } from './clients/components/front/front-header/fr
 import { FrontNavComponent } from './clients/components/front/front-nav/front-nav.component';
 import { FrontSlideShowComponent } from './clients/components/front/front-slide-show/front-slide-show.component';
 import { FrontGridCarouselComponent } from './clients/components/front/front-grid-carousel/front-grid-carousel.component';
+import { FrontFooterComponent } from './clients/components/front/front-footer/front-footer.component';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -111,7 +112,8 @@ export function createTranslateLoader(http: HttpClient) {
     FrontNavComponent,
     FrontNavComponent,
     FrontSlideShowComponent,
-    FrontGridCarouselComponent
+    FrontGridCarouselComponent,
+    FrontFooterComponent
   ],
   imports: [
     BrowserModule,
