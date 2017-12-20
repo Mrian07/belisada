9016a908d441d1ae63dd3ce84f197140ad1ab108
unknown <lalang gumirang>
2017-12-20 17:07:52 +0700

    footer

--- src/app/clients/components/front/front-footer/front-footer.component.ts
+++ src/app/clients/components/front/front-footer/front-footer.component.ts
@@ -18,4 +18,10 @@ export class FrontFooterComponent implements OnInit {
     el.style.display = el.style.display == 'none' || el.style.display == '' ? 'block' : 'none';
   }
 
+  btnTop() {
+    // document.body.scrollTop = 0; // For Safari
+    // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    $('html, body').animate({scrollTop: 0}, 1000);
+          return false;
+  }
 }
