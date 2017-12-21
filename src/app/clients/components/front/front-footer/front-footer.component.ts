ef2816e6c541892cbf9fe0c2093369faa188053f
obbie <wahyu.suit@myacico.com>
2017-12-21 11:10:32 +0700

    fixing footer

--- src/app/clients/components/front/front-footer/front-footer.component.ts
+++ src/app/clients/components/front/front-footer/front-footer.component.ts
@@ -21,7 +21,7 @@ export class FrontFooterComponent implements OnInit {
   btnTop() {
     // document.body.scrollTop = 0; // For Safari
     // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    $('html, body').animate({scrollTop: 0}, 1000);
+    // $('html, body').animate({scrollTop: 0}, 1000);
           return false;
   }
 }
