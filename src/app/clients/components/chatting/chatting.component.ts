66d02dad7f78c61bd3d56cbf97b0116a19cf108a
Achmad Ricky Budianto <achmadricky96@gmail.com>
2017-12-27 11:55:45 +0700

    Chatting UI

--- src/app/clients/components/chatting/chatting.component.ts
+++ src/app/clients/components/chatting/chatting.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, transition, style, animate, state} from '@angular/animations';
 
 @Component({
   selector: 'app-chatting',
+  animations: [
+    trigger(
+      'myAnimation',
+      [
+        transition(
+        ':enter', [
+          style({opacity: 0}),
+          animate('200ms', style({opacity: 1}))
+        ]
+      ),
+        transition(
+        ':leave', [
+          style({opacity: 1}),
+          animate('200ms', style({opacity: 0}))
+        ]
+      )]
+    )
+  ],
   templateUrl: './chatting.component.html',
   styleUrls: ['./chatting.component.scss']
 })
 export class ChattingComponent implements OnInit {
+
+  show: Boolean = false;
 
   constructor() { }
 
