63b864796d530baf8fff96edb838229260978c00
unknown <lalang gumirang>
2018-01-02 16:29:29 +0700

    profile

--- src/app/views/sellers/component/profile/profile.component.ts
+++ src/app/views/sellers/component/profile/profile.component.ts
@@ -212,6 +212,34 @@ export class ProfileComponent implements OnInit {
     fr.readAsDataURL(f);
   }
 
+  setCanvasNpwp(e) {
+    if (!this.updateImg) { return false; }
+    const cnv = document.createElement('canvas');
+    const el = e.path[0];
+    const w = el.width;
+    const h = el.height;
+
+    cnv.width = w;
+    cnv.height = h;
+    cnv.getContext('2d').drawImage(el, 0, 0, w, h);
+
+    this.newImage = cnv.toDataURL('image/jpeg', 0.5).slice(23).replace(' ', '+');
+    console.log('newImg:', this.newImage);
+  }
+
+  setUrlNpwp(event, img) {
+    const fr = new FileReader();
+    const f = event.target.files[0];
+    const that = this;
+
+    if (!f.type.match(/image.*/)) { return alert('Not valid image file'); }
+    fr.onload = function() {
+      that.updateImg = true;
+      img.src = fr.result;
+    };
+    fr.readAsDataURL(f);
+  }
+
   getProvince() {
     // Country ID harcoded to Indonesia
     this.masterService.getProvince('209').subscribe(data => {
