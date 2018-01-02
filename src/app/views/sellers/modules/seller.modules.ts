856dd365f49a6582a899033bc7733dd2e60ae2cd
nendy <nendi.ari@myacico.com>
2018-01-02 15:14:10 +0700

    chat logout, env, service

--- src/app/views/sellers/modules/seller.modules.ts
+++ src/app/views/sellers/modules/seller.modules.ts
@@ -52,6 +52,7 @@ import { AddproductService } from '../../../core/service/addproduct/addproduct.s
 import { DashboardComponent } from '../component/dashboard/dashboard.component';
 import { ProductReducer, StoreReducer, BankReducer } from '../../../store/reducers';
 import { ProductEffects } from '../../../store/effects/index';
+import { ChatService } from '../../../core/service/chat/chat.service';
 
 
 @NgModule({
@@ -114,7 +115,8 @@ import { ProductEffects } from '../../../store/effects/index';
     ChangePasswordService,
     StoreService,
     RekeningSService,
-    AddproductService
+    AddproductService,
+    ChatService
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
