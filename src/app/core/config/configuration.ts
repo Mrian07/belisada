a155501c19d9e43647587edcb38094c21489073d
Achmad Ricky Budianto <achmadricky96@gmail.com>
2018-01-04 16:04:48 +0700

    change configuration API NET to HTTPS

--- src/app/core/config/configuration.ts
+++ src/app/core/config/configuration.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
 export class Configuration {
   serverWithApiUrl = 'https://api.myacico.co.id/dev';
   serverWithAccUrl = 'http://35.185.183.169:8080/belisada-dev';
-  serverWithNetUrl = 'http://dev-api-net.belisada.co.id:8080/belisada-dev';
+  serverWithNetUrl = 'https://dev-api-net.belisada.co.id/belisada-dev/';
   serverWithImgUrl = 'https://storage.googleapis.com/myacicoproductimg';
 }
