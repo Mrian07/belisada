61323c59a257c28f3b8b49ac6ceb9a277709d085
unknown <lalang gumirang>
2018-01-10 16:21:02 +0700

    header

--- src/app/views/buyer/component/sidebar-buyer/sidebar-buyer.component.ts
+++ src/app/views/buyer/component/sidebar-buyer/sidebar-buyer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../../../../core/model/category';
+import { Category2 } from '../../../../core/model/category2';
+import { CategoryService } from '../../../../core/service/category/category.service';
 
 @Component({
   selector: 'app-sidebar-buyer',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarBuyerComponent implements OnInit {
 
-  constructor() { }
+  c1: Category[];
+  c2: Category2[];
+  imgTop: any;
+  navigationObjects: any[] = [];
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.getNavigationCategory();
+  }
+
+  getCategoryOne(cb) {
+    this.categoryService.CategoryOne().subscribe(data => {
+      this.c1 = data;
+      cb();
+    });
+  }
+
+  getCategoryTwo(categoryOneId, cb) {
+    this.categoryService.CategoryTwo(categoryOneId).subscribe(data => {
+      this.c2 = data;
+      cb();
+    });
+  }
+
+  getNavigationCategory() {
+    this.getCategoryOne(() => {
+      this.c1.forEach((item, index) => {
+        // console.log('item: ', item);
+        this.navigationObjects.push(item);
+        this.getCategoryTwo(item.mProductCategoryId, () => {
+          this.navigationObjects[index]['c2'] = this.c2;
+        });
+        // console.log('this.navigationObject: ', this.navigationObject);
+      });
+    });
   }
 
 }
