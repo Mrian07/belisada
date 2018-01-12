7b450646b01c28838b09a8e08ca05683ec88b9f8
Achmad Ricky Budianto <achmadricky96@gmail.com>
2018-01-12 14:52:33 +0700

    Shopping cart

--- src/app/views/front/component/cart/cart.component.ts
+++ src/app/views/front/component/cart/cart.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../../../../core/service/shopping-cart/shopping-cart.service';
+import { ProductService } from '../../../../core/service/product/product.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Product } from '../../../../core/model/product';
+import { CartItem } from '../../../../core/model/shoppingcart/cart-item';
+import { Observable } from 'rxjs/Observable';
+import { ShoppingCart } from '../../../../core/model/shoppingcart/shoppnig-cart';
+
+interface ICartItemWithProduct extends CartItem {
+  product: Product;
+  totalCost: number;
+}
 
 @Component({
   selector: 'app-cart',
@@ -7,12 +19,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  public cart: Observable<ShoppingCart>;
+  public cartItems: ICartItemWithProduct[] = [];
+  public itemCount: number;
+
+  private cartSubscription: Subscription;
+
+  constructor(
+    private shoppingCartService: ShoppingCartService,
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
+    window.scrollTo(0, 0);
+    this.cart = this.shoppingCartService.get();
+    this.cartSubscription = this.cart.subscribe((cart) => {
+      this.itemCount = cart.items.map((x) => x.quantity).reduce((p, n) => p + n, 0);
+      this.cartItems = [];
+      cart.items.forEach(item => {
+        this.productService.get(item.productId).subscribe((product) => {
+          // const product = prod;
+          this.cartItems.push({
+            ...item,
+            product,
+            totalCost: product.pricelist * item.quantity });
+            console.log('this.cartItems: ', this.cartItems);
+        });
+      });
+    });
   }
 
-  delete() {
-    alert('delete');
+  public updateQuantity(event: any, item: any) {
+    let quantity = 0;
+
+    if (item.quantity > event.target.value) {
+      quantity = -(item.quantity - event.target.value);
+    } else {
+      quantity = +(event.target.value - item.quantity);
+    }
+    this.shoppingCartService.updateQuantity(item.product.productId, quantity);
+  }
+
+  public removeProductFromCart(productId: number, quantity: number): void {
+    this.shoppingCartService.addItem(productId, -quantity);
   }
 }
