ff6a7127f16ef80cd189e617140b2fe974c22f86
unknown <lalang gumirang>
2018-01-15 11:01:15 +0700

    product top dan tulisan belisada.com jadi .co.id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,6 +50,7 @@ import { PaymentMethodComponent } from './views/front/component/payment-method/p
 import { ConfirmOrderComponent } from './views/front/component/confirm-order/confirm-order.component';
 import { FinihOrderComponent } from './views/front/component/finih-order/finih-order.component';
 import { AddShippingComponent } from './views/front/component/add-shipping/add-shipping.component';
+import { ProductTopBuyerComponent } from './views/buyer/component/product-top-buyer/product-top-buyer.component';
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
@@ -98,7 +99,8 @@ export function getAuthServiceConfigs() {
     PaymentMethodComponent,
     ConfirmOrderComponent,
     FinihOrderComponent,
-    AddShippingComponent
+    AddShippingComponent,
+    ProductTopBuyerComponent
   ],
   imports: [
     BrowserModule,
