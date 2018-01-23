fed0036ff5a4204f6f329d4e61780a9f13af9213
unknown <lalang gumirang>
2018-01-23 08:52:52 +0700

    upgrade seller

--- src/app/views/buyer/component/seller-propose/seller-propose.component.ts
+++ src/app/views/buyer/component/seller-propose/seller-propose.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import swal from 'sweetalert2';
+import { UpgradeService } from './../../../../core/service/upgrade/upgrade.service';
 
 @Component({
   selector: 'app-seller-propose',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerProposeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private upgradeService: UpgradeService,
+  ) { }
 
   ngOnInit() {
+
   }
 
+  agree() {
+
+    const user = JSON.parse(localStorage.user);
+    this.router.navigateByUrl('/buyer/dashboard');
+
+    if (user.role === 3 || user.role === 2) {
+      this.router.navigateByUrl('/seller/dashboard');
+    } else {
+
+        const data = {
+          userType: '1'
+        };
+        this.upgradeService.upToSeller(data).subscribe(response => {
+          this.router.navigateByUrl('/seller/dashboard');
+        });
+
+    }
+
+
+
+  }
 }
