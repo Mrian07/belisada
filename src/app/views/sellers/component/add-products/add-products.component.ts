75437910dbd20fd5b6c039b58dbbb1ad1ca13465
budi.kusuma <bdkusuma@gmail.com>
2018-01-30 16:52:04 +0700

    profile update + add product

--- src/app/views/sellers/component/add-products/add-products.component.ts
+++ src/app/views/sellers/component/add-products/add-products.component.ts
@@ -80,6 +80,10 @@ export class AddProductsComponent implements OnInit {
   guaranteeDays: any;
   news: any;
   selectedColor = [];
+  fm: any = {};
+  updateImg: any;
+  userImageNPWP: string;
+  imgNpwp: any;
   warnanya = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'pulple', 'pink', 'brown', 'grey', 'black']
 
   countries = [
@@ -508,5 +512,30 @@ export class AddProductsComponent implements OnInit {
       '<i class="fa fa-thumbs-down"></i>',
       cancelButtonAriaLabel: 'Thumbs down',
     });
+  }
+  setUrl(event, img) {
+    const fr = new FileReader();
+    const f = event.target.files[0];
+    const that = this;
+
+    if (!f.type.match(/image.*/)) { return alert('Not valid image file'); }
+    fr.onload = function() {
+      that.updateImg = true;
+      img.src = fr.result;
+    };
+    fr.readAsDataURL(f);
+  }
+  setCanvas(e, newIMG) {
+    if (!this.updateImg) { return false; }
+    const cnv = document.createElement('canvas');
+    const el = e.target;
+    const w = el.width;
+    const h = el.height;
+
+    cnv.width = w;
+    cnv.height = h;
+    cnv.getContext('2d').drawImage(el, 0, 0, w, h);
+
+    this.fm[newIMG] = cnv.toDataURL('image/jpeg', 0.5).slice(23).replace(' ', '+');
   }
 }
