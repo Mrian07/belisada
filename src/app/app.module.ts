3e5b303f959c2ff85d5988ce3216ecd1b04bb6a5
budi.kusuma <bdkusuma@gmail.com>
2018-01-30 13:56:22 +0700

    service worker disabled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -169,9 +169,9 @@ export function getAuthServiceConfigs() {
   imports: [
     BrowserModule,
     SocialLoginModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', {
-      enabled: environment.production,
-    }),
+    // ServiceWorkerModule.register('/ngsw-worker.js', {
+    //   enabled: environment.production,
+    // }),
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
