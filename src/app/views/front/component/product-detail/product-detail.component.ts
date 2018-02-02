5567d502e1e909c0bfb1a00584ff4778c8186eb3
KF <kf@KFs-MacBook-Pro.local>
2018-02-02 10:22:13 +0700

    rating fix dan bahsa dan hilangkan base64 console

--- src/app/views/front/component/product-detail/product-detail.component.ts
+++ src/app/views/front/component/product-detail/product-detail.component.ts
@@ -50,6 +50,7 @@ export class ProductDetailComponent implements OnInit {
   diskon2: any;
   popx: any;
   diskon3: any;
+  rate: number;
   allRate: number;
   // percent: any;
   ProductList: ProductDetail = new ProductDetail();
@@ -133,7 +134,7 @@ export class ProductDetailComponent implements OnInit {
       .subscribe(data => {
         if (data.detail !== undefined) {
           this.ProductList = data.detail;
-          console.log('brow', this.ProductList);
+          // console.log('brow', this.ProductList);
           //console.log(this.ProductList.mBpartnerStoreId);
           //this.saveSearch(this.ProductList.productId, this.ProductList.name);
           const garansi = this.garansiDay.find(x => x.day === this.ProductList.guaranteeDays);
