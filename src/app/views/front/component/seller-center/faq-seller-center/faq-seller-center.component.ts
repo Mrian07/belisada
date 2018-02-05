c22cd52fdb1702d51678891c2c0fd08fce67162b
KF <kf@KFs-MacBook-Pro.local>
2018-02-05 17:04:32 +0700

    seller center dan faq

---
+++ src/app/views/front/component/seller-center/faq-seller-center/faq-seller-center.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-seller-center',
+  templateUrl: './faq-seller-center.component.html',
+  styleUrls: ['./faq-seller-center.component.scss']
+})
+export class FaqSellerCenterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
