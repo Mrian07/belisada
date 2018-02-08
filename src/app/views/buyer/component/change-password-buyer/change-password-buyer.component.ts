9b17d75fcb9729050066216c032056e6fb786cbc
Achmad Ricky Budianto <achmad.budianto@myacico.co.id>
2018-02-08 11:51:57 +0700

    Merge DANGER! if any error happened please tell me.

--- src/app/views/buyer/component/change-password-buyer/change-password-buyer.component.ts
+++ src/app/views/buyer/component/change-password-buyer/change-password-buyer.component.ts
@@ -12,9 +12,43 @@ export class ChangePasswordBuyerComponent implements OnInit {
   changePassword: any;
   token: any;
   role: any;
+  viewPass: Boolean = false;
+  viewPass2: Boolean = false;
+  viewPass3: Boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  togglePass() {
+    this.viewPass = !this.viewPass;
+    const el = (<HTMLInputElement>document.getElementById('password'));
+    if (this.viewPass) {
+      el.type = 'text';
+    } else {
+      el.type = 'password';
+    }
+  }
+
+  togglePass2() {
+    this.viewPass2 = !this.viewPass2;
+    const el2 = (<HTMLInputElement>document.getElementById('password2'));
+    if (this.viewPass2) {
+      el2.type = 'text';
+    } else {
+      el2.type = 'password';
+    }
+  }
+
+  togglePass3() {
+    this.viewPass3 = !this.viewPass3;
+    const el3 = (<HTMLInputElement>document.getElementById('password3'));
+    if (this.viewPass3) {
+      el3.type = 'text';
+    } else {
+      el3.type = 'password';
+    }
   }
 
 }
