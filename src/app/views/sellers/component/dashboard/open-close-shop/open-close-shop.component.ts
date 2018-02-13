df89709ac2912a7f0b696016c24f36313ac2c49f
unknown <lalang gumirang>
2018-02-13 11:43:16 +0700

    open close

--- src/app/views/sellers/component/dashboard/open-close-shop/open-close-shop.component.ts
+++ src/app/views/sellers/component/dashboard/open-close-shop/open-close-shop.component.ts
@@ -2,6 +2,7 @@ import { StoreService } from './../../../../../core/service/store/store.service'
 import { Component, OnInit } from '@angular/core';
 import { FlagService } from '../../../../../core/service/flag.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { constants } from 'fs';
 
 @Component({
   selector: 'app-open-close-shop',
@@ -14,14 +15,34 @@ export class OpenCloseShopComponent implements OnInit {
   dateEnd: FormControl;
   isOffDay: FormControl;
   mBpartnerStoreId: FormControl;
+  dayOffNote: FormControl;
   modal: boolean;
+  storeId: number;
+  isOff: string;
+  stores: any[] = [];
   constructor(
     private storeService: StoreService,
     private flagService: FlagService
   ) { }
 
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+    this.getAllStore();
+  }
 
+  getAllStore() {
+    this.storeService.getAll().subscribe(response => {
+      console.log('getAllStore response: ', response);
+      this.stores = response;
+      this.isOff = 'Y';
+
+      this.isOffDay.setValue('Y');
+      this.mBpartnerStoreId.setValue(response[0].mBpartnerStoreId);
+
+      this.storeId = response[0].mBpartnerStoreId;
+      // console.log('apa ini lah', response[0].mBpartnerStoreId);
+    });
   }
 
   createFormControls() {
@@ -29,6 +50,7 @@ export class OpenCloseShopComponent implements OnInit {
     this.dateEnd = new FormControl('');
     this.isOffDay = new FormControl('');
     this.mBpartnerStoreId = new FormControl('');
+    this.dayOffNote = new FormControl('');
   }
 
   createForm() {
@@ -37,12 +59,13 @@ export class OpenCloseShopComponent implements OnInit {
       dateEnd: this.dateEnd,
       isOffDay: this.isOffDay,
       mBpartnerStoreId: this.mBpartnerStoreId,
+      dayOffNote: this.dayOffNote,
     });
   }
 
-  closeModalShop() {
-    this.flagService.changeMessage('close-popup');
-  }
+  // closeModalShop() {
+  //   this.flagService.changeMessage('close-popup');
+  // }
 
   onSubmit() {
     const model = this.createComForm.value;
@@ -50,9 +73,11 @@ export class OpenCloseShopComponent implements OnInit {
       dateStart: model.dateStart,
       dateEnd: model.dateEnd,
       isOffDay: model.isOffDay,
-      mBpartnerStoreId: model.mBpartnerStoreId
+      mBpartnerStoreId: model.mBpartnerStoreId,
+      dayOffNote: model.dayOffNote,
     };
-
+    console.log(data);
+    this.flagService.changeMessage('close-popup');
     this.storeService.openClose(data).subscribe(response => {
 
       // if (response.status === '1') {
