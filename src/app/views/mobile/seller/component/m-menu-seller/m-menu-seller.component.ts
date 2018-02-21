a48919ecd615c64fd012ebac0b9aa8b06c459d0e
unknown <lalang gumirang>
2018-02-21 17:07:33 +0700

    mobile avatar buyer & seller

--- src/app/views/mobile/seller/component/m-menu-seller/m-menu-seller.component.ts
+++ src/app/views/mobile/seller/component/m-menu-seller/m-menu-seller.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import swal from 'sweetalert2';
 import { LoginService } from '../../../../../core/service/login/login.service';
 import { ChatService } from '../../../../../core/service/chat/chat.service';
+import { ProfileService } from '../../../../../core/service/profile/profile.service';
 
 @Component({
   selector: 'app-m-menu-seller',
@@ -16,12 +17,29 @@ export class MMenuSellerComponent implements OnInit {
   constructor(
     private router: Router,
     private loginService: LoginService,
-    private chat: ChatService
+    private chat: ChatService,
+    private profileService: ProfileService
   ) { }
 
   user: any;
+  userImgAvatar: string;
+  fm: any = {};
+  name: string;
+  email: string;
+
   ngOnInit() {
     this.user = this.loginService.whoLogin();
+    this.fillForms();
+  }
+
+  fillForms() {
+    const luser = JSON.parse(localStorage.getItem('user'));
+    this.profileService.getProfileBuyer(luser.token).subscribe(data => {
+
+      this.userImgAvatar = data.imageAvatar ?'data:image/png;base64,' + data.imageAvatar : '/assets/img/kristy.png';
+      this.name = data.name;
+      this.email = data.email;
+    });
   }
 
   logout() {
