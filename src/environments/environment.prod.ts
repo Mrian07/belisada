5633cf38f4fdf33eebeaadbaf9879a14e9cc86f1
NendyAR <arie@nendy.name>
2018-02-27 16:54:38 +0700

    api-net add dev env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://dev-acc-jav.belisada.co.id:8443/belisada-prod/',
+  api2Url: 'https://api-net.belisada.co.id/belisada',
   chatUrl: 'https://chat.myacico.co.id'
 };
