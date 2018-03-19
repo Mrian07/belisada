0463c1382f5f6ac9deb8172327a82a2f50cfaa4a
unknown <lalang gumirang>
2018-03-19 13:34:00 +0700

    home

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
         component: LayoutComponent,
         children: [
             {
-                path: 'home',
+                path: '',
                 component: HomeComponent,
                 data: {
                     title: 'home'
