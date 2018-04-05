a5e0ba7bacd4043a6254b0682b4d34945a24b1a2
DESKTOP-0I52AL0\Ricky <achmad.budianto@myacico.co.id>
2018-04-05 11:28:29 +0700

    account menu, logout

--- src/app/core/util/jwt.util.ts
+++ src/app/core/util/jwt.util.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class JWTUtil {
     parseJwt(token) {
+        if (!token) {
+            return {};
+        }
         const base64Url = token.split('.')[1];
         const base64 = base64Url.replace('-', '+').replace('_', '/');
         return JSON.parse(window.atob(base64));
