664b4830e4261414bf4b97e173d13e0860765377
NendyAR <arie@nendy.name>
2018-04-10 14:27:31 +0700

    create store

--- src/app/core/services/store/store.service.ts
+++ src/app/core/services/store/store.service.ts
@@ -1,4 +1,5 @@
-import { CreateStoreResponse, CreateStoreRequest } from './models/store.model';
+import { Observable } from 'rxjs/Observable';
+import { CreateStoreResponse, CreateStoreRequest, CheckStoreRequest, CheckStoreResponse } from './models/store.model';
 import { Configuration } from './../../config/configuration';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -8,10 +9,25 @@ export class StoreService {
 
   constructor(private cfg: Configuration, private http: HttpClient) { }
 
-  // param: {name: string, address: string, description: string}
+  // param: {name: string, address: string, description: string?, picture: string?}
   // used by create-store component
   create(data: CreateStoreRequest) {
     return this.http.post(this.cfg.apiURL + '/store/create', data).map(rsl => rsl as CreateStoreResponse);
+  }
+
+  // param: {name: string, address: string, description: string}
+  // used by create-store component
+  isExist(data: CheckStoreRequest): Observable<CheckStoreResponse> {
+    // return new Observable(obs => {
+    //   setTimeout(() => {
+    //     const res: CheckStoreResponse = new CheckStoreResponse();
+    //     res.status = 5;
+    //     res.message = 'toko sudah diambil orang';
+    //     obs.next(res);
+    //     obs.complete();
+    //   }, 1500);
+    // });
+    return this.http.post(this.cfg.apiURL + '/store/check', data).map(rsl => rsl as CheckStoreResponse);
   }
 
 }
