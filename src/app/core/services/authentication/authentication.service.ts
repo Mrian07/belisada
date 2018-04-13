de413e2a5383d23501cd6a3e39f45861a765959f
unknown <lalang gumirang>
2018-04-13 16:01:24 +0700

    auth

--- src/app/core/services/authentication/authentication.service.ts
+++ src/app/core/services/authentication/authentication.service.ts
@@ -6,32 +6,28 @@ import { HttpHeaders } from '@angular/common/http/src/headers';
 import { Router } from '@angular/router';
 import { Token } from './models/authentication.model';
 
-/*
-AuthenticationService.ts digunakan di app.module.ts
-*/
-
 @Injectable()
 export class AuthenticationService {
     constructor(private configuration: Configuration, private http: HttpClient, private routes: Router) { }
 
     /*
-    Fungsi ini untuk melakukan pengecekan token dari local storage ke backend.
+    param:
+    Used by: app.module.ts
+    Description: Fungsi ini untuk melakukan pengecekan token dari local storage ke backend.
     */
     checkToken() {
         const token = localStorage.getItem('token');
         const objToken = {
           token : token
         } ;
-
-        /*
-        Hasil dari token yang dicocokan dari local storage ke backend akan di maping menggunakan class Token pada authentication.model.
-        */
         return this.http.post(this.configuration.apiURL + '/account/checktoken', objToken)
         .map(resp => resp as Token);
     }
 
     /*
-    Fungsi ini mengambil token yang tersimpan pada local storage.
+    param:
+    Used by: app.module.ts
+    Description: Fungsi ini mengambil token yang tersimpan pada local storage.
     */
     getToken() {
         const token = localStorage.getItem('token');
--- src/app/core/services/authentication/authguard.ts
+++ src/app/core/services/authentication/authguard.ts
@@ -3,16 +3,16 @@ import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { CanActivateChild } from '@angular/router/src/interfaces';
 import { Router } from '@angular/router';
 
-/*
-authguard.ts digunakan pada app.routing dan app.module.ts.
-*/
 @Injectable()
 export class OnlyLoggedInUsersGuard implements CanActivateChild {
   constructor( private router: Router) {
   }
 
   /*
-  Fungsi ini untuk melakukan pengecekan apakah token ada pada local storage, jika tidak ada maka user akan dialihkan ke halaman sign in.
+  param:
+  Used by: app.module.ts
+  Description: Fungsi ini untuk melakukan pengecekan apakah token ada pada local storage,
+  jika tidak ada maka user akan dialihkan ke halaman sign in.
   */
   canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const user = localStorage.getItem('token');
