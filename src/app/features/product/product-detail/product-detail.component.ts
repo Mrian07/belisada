1b08f553659a9dc03fc991748511b3d70e5f22cc
Lalang <lalanggumirang@gmail.com>
2018-07-13 09:41:26 +0700

    rapih-rapih checkout

--- src/app/features/product/product-detail/product-detail.component.ts
+++ src/app/features/product/product-detail/product-detail.component.ts
@@ -44,7 +44,6 @@ export class ProductDetailComponent implements OnInit {
     this.activeSpesifikasi = true;
     this.activatedRoute.params.subscribe((params: Params) => {
       this.productService.detailProduct(params['id']).subscribe(res => {
-        console.log('Produk', res);
         this.productDetail = res.data;
         this.moreInformation = res.data.moreInformation;
         this.tabVal = this.productDetail.specification;
