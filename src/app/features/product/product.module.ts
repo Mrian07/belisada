bdee4c16fba9a54b753e252ed3479e961025014f
macbook <macbook@Lalang-MacBook-Pro.local>
2018-08-07 16:30:43 +0700

    penawaran lain

--- src/app/features/product/product.module.ts
+++ src/app/features/product/product.module.ts
@@ -7,14 +7,11 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
 
 import { ThemeModule } from '../../theme/theme.module';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { EtalaseTokoComponent } from '@belisada/features/buyer/store/etalase-toko/etalase-toko.component';
 import { AnotherOfferComponent } from './another-offer/another-offer.component';
-=======
 import { ShareModule } from '@ngx-share/core';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
->>>>>>> 3226ff7dec0c9cb89e7441a3d8d45bccf998412b
 
 @NgModule({
   imports: [
