0c52e1fa8b866e97f449e2f2c1327b382740c15e
madrick <achmad.budianto@myacico.co.id>
2018-09-17 14:53:45 +0700

    Home wrong condition, sing-in redesign

--- src/app/theme/layout/auth/auth-layout.component.ts
+++ src/app/theme/layout/auth/auth-layout.component.ts
@@ -5,22 +5,26 @@ import { Globals } from '@belisada/core/services';
   selector: 'bs-auth-layout',
   template: `
     <div class="daddy-container">
-        <div class="overlay-header" *ngIf="globals.isBackdropActive === true"></div>
-        <app-header></app-header>
-        <div class="wrapper-authentication containPage">
+      <div class="overlay-header" *ngIf="globals.isBackdropActive === true"></div>
+      <app-header></app-header>
+      <div class="container-fluid has-header has-footer">
+        <div class="row">
+          <div class="col-7">
             <bs-side-auth></bs-side-auth>
+          </div>
 
-            <div>
-                <router-outlet></router-outlet>
-            </div>
+          <div class="col-5">
+            <router-outlet></router-outlet>
+          </div>
         </div>
+      </div>
 
-        <app-footer></app-footer>
+      <app-footer></app-footer>
     </div>`
 })
 export class AuthLayoutComponent {
 
-    constructor(public globals: Globals) {
-    }
+  constructor(public globals: Globals) {
+  }
 
 }
