b22b4545b75fdcc70d81d56a4da10b27c2582e75
Lalang <lalang@myacico.co.id>
2019-02-06 11:19:29 +0700

    review buyer

--- src/app/core/services/review/review.service.ts
+++ src/app/core/services/review/review.service.ts
@@ -3,7 +3,7 @@ import { HttpParams, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Configuration } from '@belisada/core/config';
 import { map } from 'rxjs/operators';
-import { ListReview , ListReviewReq} from '@belisada/core/models/review/review.model';
+import { ListReview , ListReviewReq, ReviewBuyer} from '@belisada/core/models/review/review.model';
 
 @Injectable({
     providedIn: 'root',
@@ -24,6 +24,17 @@ constructor(private configuration: Configuration, private http: HttpClient) { }
         return this.http.post(this.configuration.apiURL + '/productfeedback/review/create', data)
         .pipe(
             map(response => response as ListReview)
+        );
+    }
+
+    getReviewBuyer(queryParams?): Observable<ReviewBuyer> {
+        let params = new HttpParams();
+        Object.keys(queryParams).forEach(function(k) {
+            params = params.append(k, queryParams[k]);
+        });
+        return this.http.get(this.configuration.apiURL + '/productfeedback/review/buyer', {params: params})
+        .pipe(
+            map(response => response as ReviewBuyer)
         );
     }
 }
--- src/app/features/buyer/review-product/review-product.component.ts
+++ src/app/features/buyer/review-product/review-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ReviewService } from '@belisada/core/services/review/review.service';
+import { ReviewBuyer } from '../../../core/models/review/review.model';
+import { Router, ActivatedRoute, Params, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-review-product',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewProductComponent implements OnInit {
 
-  constructor() { }
+  reviewBuyer = new ReviewBuyer();
+
+  constructor(
+    private reviewService: ReviewService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.loadData();
+    console.log('gaban ini');
+  }
+
+  loadData() {
+    const queryParams = {
+      itemperpage: 10,
+      page: 1
+    };
+
+    this.reviewService.getReviewBuyer(queryParams).subscribe(response => {
+      console.log('data', response);
+      this.reviewBuyer = response;
+    });
+  }
+
+  detail(id, name) {
+    this.router.navigate(['/product/product-detail/' + id + '/' + name]);
   }
 
 }
