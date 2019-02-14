44b3a15f4ada010837e6604975b1cc88b11c457f
Achmad Ricky Budianto <achmadricky96@gmail.com>
2019-02-14 08:45:13 +0700

    chat and banner

--- src/app/features/chat/chat.module.ts
+++ src/app/features/chat/chat.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChatRoutingModule } from './chat-routing.module';
-import { ChatComponent } from './chat.component';
 import { ThemeModule } from 'app/theme/theme.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -17,6 +16,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
   ],
-  declarations: [ChatComponent]
+  declarations: []
 })
 export class ChatModule { }
