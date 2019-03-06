b7ff99d82723f80f9bc286d2a9cd5586022b5cbb
Achmad Ricky Budianto <achmadricky96@gmail.com>
2019-03-06 18:00:28 +0700

    order status bugs

--- src/app/features/buyer/order/order.component.ts
+++ src/app/features/buyer/order/order.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TransactionService } from '@belisada/core/services/transaction/transaction.service';
+import { ContentOrderStatus } from '@belisada/core/models/transaction/transaction.model';
+import { LoadingService } from '@belisada/core/services/globals/loading.service';
 
 @Component({
   selector: 'app-order',
@@ -8,22 +11,69 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class OrderComponent implements OnInit {
 
-  tabOrder = 'ALL';
+  public isLoading: Boolean = false;
+
+  public activeTab: String = 'ALL';
+  public currentPage: Number = 1;
+
+  /**
+   * variable that used in another component
+   */
+  public order: ContentOrderStatus = new ContentOrderStatus();
+
   constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
+    private _loadingService: LoadingService,
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+    private _transactionService: TransactionService
   ) {
-      this.activatedRoute.queryParams.subscribe((queryParam) => {
-      this.tabOrder = (queryParam.status) ? queryParam.status : 'ALL';
+    this._activatedRoute.queryParams.subscribe((queryParam) => {
+      this.activeTab = (queryParam.status) ? queryParam.status : 'ALL';
+      this.currentPage = (queryParam.page) ? queryParam.page : 1;
+      if (this.activeTab === '133') this._orderList(this.activeTab);
     });
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public changeTab(data) {
+    this.activeTab = data;
+    this._router.navigate(['/buyer/order'], { queryParams: {page: 1, status: data}, queryParamsHandling: 'merge' });
   }
 
-  tab(data) {
-    this.tabOrder = data;
-    this.router.navigate(['/buyer/order'], { queryParams: {page: 1, status: data}, queryParamsHandling: 'merge' });
+  /**
+   * Get order list for order-status.component.ts
+   * @param statusOrderCode
+   */
+  private _orderList(statusOrderCode?: any) {
+    const queryParams = {
+      itemperpage: 10,
+      page: this.currentPage,
+      transaction_status: statusOrderCode
+    };
+    this._loadingService.show();
+    this._transactionService.getOrder(queryParams).subscribe(response => {
+      this._loadingService.hide();
+      this.order = response;
+      // const b =  respon.content.filter(x => x.expiredConfirmationPaymentBuyerDate !== '');
+      // this.proddetail = respon;
+      // this.list = respon.content;
+      // b.forEach((x) => {
+      //   mct.countdown(x.expiredConfirmationPaymentBuyerDate, (countdown) => {
+      //     this.proddetail.content.find(i => i.paymentNumber === x.paymentNumber).countdown = countdown;
+      //   });
+      // });
+      // this.proddetail = respon;
+      // this.a = respon.totalElements;
+      // this.pages = [];
+      // this.lastPage = this.proddetail.totalPages;
+      // for (let r = (this.currentPage - 3); r < (this.currentPage - (-4)); r++) {
+      //   if (r > 0 && r <= this.proddetail.totalPages) {
+      //     this.pages.push(r);
+      //   }
+      // }
+      // this.isLoading = false;
+    });
   }
 
 }
