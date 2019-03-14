cd00432eec14799c05b77c5433592753531958e8
Lalang <lalang@myacico.co.id>
2019-03-14 11:44:21 +0700

    update design success online payment

--- src/app/features/onlinepayment/onlinepayment-success/onlinepayment-success.component.ts
+++ src/app/features/onlinepayment/onlinepayment-success/onlinepayment-success.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { CheckoutService } from '@belisada/core/services/checkout/checkout.service';
+import { SuccessTransactionRes, SuccessTransactionData } from '@belisada/core/models/checkout/checkout-transaction';
 
 @Component({
   selector: 'app-onlinepayment-success',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OnlinepaymentSuccessComponent implements OnInit {
 
-  constructor() { }
+  successTransactionRes: SuccessTransactionRes = new SuccessTransactionRes();
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private checkoutService: CheckoutService,
+  ) {
+    this.successTransactionRes.data = new SuccessTransactionData();
+  }
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+
+      this.checkoutService.getSuccessTransaction(params['order_id']).subscribe(response => {
+        this.successTransactionRes = response;
+        const arrD = response.data.expiredTime.split(' ')[0].split('/');
+        const newDate = arrD[2] + '-' + arrD[1] + '-' + arrD[0];
+
+      });
+    });
   }
 
 }
