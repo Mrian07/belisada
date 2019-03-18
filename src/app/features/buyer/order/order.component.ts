28d14fac997829a53c9de0cba601ee0588602c45
Lalang <lalang@myacico.co.id>
2019-03-18 10:08:28 +0700

    grand total view (api not ready yet)

--- src/app/features/buyer/order/order.component.ts
+++ src/app/features/buyer/order/order.component.ts
@@ -54,6 +54,7 @@ export class OrderComponent implements OnInit {
     };
     this._loadingService.show();
     this._transactionService.getOrder(queryParams).subscribe(response => {
+
       this._loadingService.hide();
       this.order = response;
       // const b =  respon.content.filter(x => x.expiredConfirmationPaymentBuyerDate !== '');
