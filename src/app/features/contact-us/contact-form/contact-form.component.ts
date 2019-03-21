008e012e0077adfea768c81523a5fab8f9d0e04f
Abdul Kadir <frenchfriespeople@gmail.com>
2019-03-21 10:51:17 +0700

    prototype contact us (not working yet) bug

--- src/app/features/contact-us/contact-form/contact-form.component.ts
+++ src/app/features/contact-us/contact-form/contact-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, NgForm } from '@angular/forms';
+import { ContactUsModel } from '@belisada/core/models';
+// import { ContactUsService } from '@belisada/core/services';
+import swal from 'sweetalert2';
+import { ContactUsService } from '@belisada/core/services';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-
-  constructor() { }
+  public contactForm: FormGroup;
+  name: FormControl;
+  email: FormControl;
+  message: FormControl;
+  constructor(
+    private fb: FormBuilder,
+    // private contactusservice: ContactUsService,
+    private contactUsService: ContactUsService
+  ) { }
 
   ngOnInit() {
+    this.contactForm = this.fb.group({
+    email: [null, Validators.required, Validators.email],
+    message: [null, [Validators.required]],
+    name: [null, [Validators.required]]
+    });
   }
+  onSubmit() {
+    const contactusmodel: ContactUsModel = new ContactUsModel;
+    contactusmodel.name = this.contactForm.controls['email'].value;
+    contactusmodel.name = this.contactForm.controls['message'].value;
+    contactusmodel.name = this.contactForm.controls['name'].value;
+    console.log('contact us model:', contactusmodel);
 
+    return;
+
+    this.contactUsService.insert(contactusmodel).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
